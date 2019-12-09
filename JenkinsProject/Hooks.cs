a040e6e8f7ccfacb6ba5cfc84cef47a886afb833
Mehrudin Herceg <mehrudin.herceg@gmail.com>
2019-12-09 18:57:23 +0100

    first commit

---
+++ JenkinsProject/Hooks.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsProject
+{
+
+    //Enum for browserType
+    public enum BrowerType
+    {
+        Chrome,
+        Firefox,
+        IE
+    }
+
+    public class Hooks : Base
+    {
+        private BrowerType _browserType;
+
+        [SetUp]
+        public void InitializeTest()
+        {
+            //Get the value from NUnit-console --params 
+            //e.g. nunit3-console.exe --params:Browser=Firefox \SeleniumNUnitParam.dll
+            //If nothing specified, test will run in Chrome browser
+            // var browserType = TestContext.Parameters.Get("Browser", "Chrome");
+            var browserType = Environment.GetEnvironmentVariable("browser");
+            //Parse the browser Type, since its Enum
+            _browserType = (BrowerType)Enum.Parse(typeof(BrowerType), browserType);
+            //Pass it to browser
+            ChooseDriverInstance(_browserType);
+        }
+
+        private void ChooseDriverInstance(BrowerType browserType)
+        {
+            if (browserType == BrowerType.Chrome)
+                Driver = new ChromeDriver();
+            else if (browserType == BrowerType.Firefox)
+            {
+                //FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\Users\840\Desktop\drivers");
+                //service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox";
+                Driver = new FirefoxDriver();
+
+            }
+            else if (browserType == BrowerType.IE)
+            {
+                Driver = new InternetExplorerDriver();
+            }
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            Driver.Quit();
+        }
+    }
+}
+
---
+++ JenkinsProject/Tests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsProject
+{
+    public class Tests:Hooks
+    {
+        [Test]
+        public void GoogleTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("Selenium");
+            System.Threading.Thread.Sleep(5000);
+            Driver.FindElement(By.Name("btnK")).Click();
+            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                                            "The text selenium doest not exist");
+
+        }
+        [Test]
+        public void GoogleTest2()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("Selenium");
+            System.Threading.Thread.Sleep(5000);
+            Driver.FindElement(By.Name("btnK")).Click();
+            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                                            "The text selenium doest not exist");
+
+        }
+    }
+}
